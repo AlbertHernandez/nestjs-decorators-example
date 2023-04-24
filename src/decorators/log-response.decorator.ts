ffde0183945b56e142f4639d9ffea984f8b1cbf0
Albert Hernandez <alberthernandezdev@gmail.com>
2023-04-24 12:01:07 +0200

    feat: access to nestjs providers

--- src/decorators/log-response.decorator.ts
+++ src/decorators/log-response.decorator.ts
@@ -1,14 +1,20 @@
+import { Inject, Logger } from '@nestjs/common';
+
 export function LogResponse() {
+  const injectLogger = Inject(Logger); // creates a function that injects a Logger into the decorated class
+
   return (
     target: any,
     propertyKey: string,
     propertyDescriptor: PropertyDescriptor,
   ) => {
+    injectLogger(target, 'logger'); // this is the same as using constructor(private readonly logger: LoggerService) in a class
     const originalMethod = propertyDescriptor.value;
 
     propertyDescriptor.value = async function (...args: any[]) {
       const result = await originalMethod.apply(this, args);
-      console.log(
+      const logger: Logger = this.logger;
+      logger.log(
         `[${target.constructor.name} - ${propertyKey}] Response:`,
         result,
       );
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,9 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService, Logger],
 })
 export class UsersModule {}
