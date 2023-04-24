50c5ec291cd905af881a8f00840fcceeec414909
Albert Hernandez <alberthernandezdev@gmail.com>
2023-04-24 12:18:54 +0200

    feat: add example of how it looks without decorators

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { LogResponse } from '../decorators/log-response.decorator';
 import { HandleError } from '../decorators/handle-error.decorator';
+import { ErrorHandlerService } from '../error-handler/error-handler.service';
 
 @Injectable()
 export class UsersService {
+  constructor(
+    private readonly usersLogger: Logger,
+    private readonly usersErrorHandlerService: ErrorHandlerService,
+  ) {}
+
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
@@ -22,11 +28,25 @@ export class UsersService {
     return `This action returns a #${id} user`;
   }
 
+  // Example logger without decorator
   update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+    const response = `This action updates a #${id} user`;
+    this.usersLogger.log(
+      `[${this.constructor.name} - update] Response: ${response}`,
+    );
+    return response;
   }
 
+  // Example error handler without decorator
   remove(id: number) {
-    return `This action removes a #${id} user`;
+    try {
+      throw new Error('Custom error');
+      return `This action removes a #${id} user`;
+    } catch (error) {
+      if (error instanceof Error) {
+        this.usersErrorHandlerService.handle(error);
+      }
+      throw error;
+    }
   }
 }
